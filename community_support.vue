e364d703286c041e0e715571e0871a345babc303
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-17 15:22:30 -0400

    Northgate Shopping Centre 1.0  /community_support.vue

--- community_support.vue
+++ community_support.vue
@@ -99,6 +99,7 @@
             },
             created(){
                this.updatePageData(this.id);
+               this.dataLoaded === true;
             },
             computed: {
                 ...Vuex.mapGetters([
