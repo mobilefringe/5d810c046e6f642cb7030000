629b0436899b097d6e179d661b6ff57a58741058
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-18 10:52:25 -0400

    Northgate Shopping Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -174,43 +174,7 @@
         		</div>
         		
         		
-        		
-        		<h2 class="home_title center">In Our Feed</h2>
-                <div class="row hidden-xs margin_60">
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="insta-feed-container">
-                            <div class="insta-feed-image " v-for="(item, index) in instaFeed">
-                                <a :href="item.link" target="_blank">
-                                    <div class="insta-img" v-bind:style="{ 'background-image': 'url(' + item.images.standard_resolution.url + ')' }"></div>
-                                    <div class="insta_content">
-                                        <p class="insta_caption">{{ item.caption.text }}</p>
-                                        <p class="insta_user">@{{ item.user.username }}</p>
-                                        <i class="insta_icon fab fa-instagram"></i>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="visible-xs margin_60 insta-feed-container">
-                    <div class="insta_prev"></div>
-                    <slick ref="slick" :options="instaOptions">
-                        <div class="insta-feed-image " v-for="(item, index) in instaFeed">
-                            <a :href="item.link" target="_blank">
-                                <div class="insta-img" v-bind:style="{ 'background-image': 'url(' + item.images.standard_resolution.url + ')' }"></div>
-                                <div class="insta_content">
-                                    <p class="insta_caption">{{ item.caption.text }}</p>
-                                    <p class="insta_user">@{{ item.user.username }}</p>
-                                    <i class="insta_icon fab fa-instagram"></i>
-                                </div>
-                            </a>
-                        </div>
-                    </slick>
-                    <div class="insta_next"></div>
-                </div>
-                
-                
-                
+       
         	</div>
         </transition>    		
 	</div>
