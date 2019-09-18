b5fef62b25c20d0047f56dcbbb3a4e3a845cbbdc
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-18 11:38:32 -0400

    Northgate Shopping Centre 1.0  /guest_services.vue

--- guest_services.vue
+++ guest_services.vue
@@ -5,10 +5,10 @@
             <div v-if="dataLoaded" v-cloak>
                 <div class="site_container">
     				<div class="pages_content">
-    				    <h2 style="background: #f9f9f9; color: #666; border-bottom: 1px solid #eee; width: 100%; font-size: 16px !important; text-align: center; padding: 15px 15px; margin: 0 -15px;">Located near Center Court</h2>
+    				    <h2 style="background: #f9f9f9; color: #666; border-bottom: 1px solid #eee; width: 100%; font-size: 16px !important; text-align: center; padding: 15px 15px; text-transform: uppercase; margin: 0 -15px;">Located near Center Court</h2>
     				    
     				    
-<h3 style="text-align: center; max-width: 600px; margin: 0 auto; font-weight: normal; letter-spacing: 1px; line-height: 1.5em; margin-top: 1em;">Now located by the <strong>Parking Garage entrance</strong></h3>
+                        <h3 style="text-align: center; max-width: 600px; margin: 0 auto; font-weight: normal; letter-spacing: 1px; line-height: 1.5em; margin-top: 1em;">Now located by the <strong>Parking Garage entrance</strong></h3>
 <ul id="services_list" class="clearfix">
 <li style="background-position: center -1219px;">Stroller, wheelchair &amp; scooter rentals</li>
 <li style="background-position: center -789px;">Portable cell phone chargers</li>
