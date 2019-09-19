991087f16261fa7421ea1393dbb87b53be0d9075
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-19 16:11:20 -0400

    Northgate Shopping Centre 1.0  /event_photos.vue

--- event_photos.vue
+++ event_photos.vue
@@ -93,7 +93,8 @@
                     try {
                         let results = await Promise.all([
                             this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }),
-                            this.$store.dispatch("getData", "repos")]);
+                            this.$store.dispatch("getData", "repos")]),
+                             this.$store.dispatch("getData", "fashions")])
                         return results;
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
