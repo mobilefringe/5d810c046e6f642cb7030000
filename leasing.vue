88916995029bb9d466560af432905dd9c7501147
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-19 00:50:59 -0400

    Northgate Shopping Centre 1.0  /leasing.vue

--- leasing.vue
+++ leasing.vue
@@ -99,7 +99,7 @@
                             
                         }
                     } else {
-                        this.pageBanner = { "image_url": "https://via.placeholder.com/1920x300" }
+                        this.pageBanner = { "image_url": "//codecloud.cdn.speedyrails.net/sites/5d810c046e6f642cb7030000/image/png/1568860718000/BackupBanner.png" }
                     }
 
 this.currentPage = response[0].data;
