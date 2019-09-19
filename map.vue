9d0099e8630f3a968e0e38e8db76ff1f17ea6c36
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-18 22:53:32 -0400

    Northgate Shopping Centre 1.0  /map.vue

---
+++ map.vue
@@ -0,0 +1,140 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loader v-if="!dataLoaded"></loader>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+        		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        			<div class="site_container">
+        				<div class="header_content caps">
+        					<h1>{{$t("map_page.header_title")}}</h1>
+        				</div>
+        			</div>
+        		</div>
+        		
+        		<div class="page_header" v-if="!pageBanner">
+        			<div class="site_container">
+        				<div class="header_content caps">
+        					<h1>{{$t("map_page.header_title")}}</h1>
+        				</div>
+        			</div>
+        		</div>
+        		
+                <div class="site_container">
+                    <div class="row margin_30">
+                        <div class="col-md-3">
+                            <div class="map_search_container">
+                                <search-component v-model="storeSearch" :list="processedStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect" :placeholder="$t('stores_page.find_your_store')">
+                                    <template slot="item" scope="option">
+                                        <article class="media"><p>{{ option.data.name }}</p></article>
+                                    </template>
+                                </search-component>
+                                <i id="store_search_icon" class="fa fa-search" aria-hidden="true"></i>
+                            </div>
+                        </div>
+                        <div class="col-md-9"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-9 col-md-push-3">
+                            <mapplic-map ref="mapplic_ref" :height="556" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-map>
+                        </div>
+                        <div class="margin-30 visible-mobile"></div>
+                        <div class="hidden_phone col-md-3 col-md-pull-9">
+                            <div class="store_list_container hidden-mobile">
+                                <div class="store_name" v-for="store in allStores">
+                                    <p v-on:click="dropPin(store)">{{store.name}}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+<script>
+    define(["Vue", "vuex", "vue!mapplic-map"], function(Vue, Vuex, MapplicComponent) {
+        return Vue.component("stores-component", {
+            template: template, // the variable template will be injected
+            data: function () {
+                return {
+                    dataLoaded: false,
+                    pageBanner: null,
+                    suggestionAttribute: "name",
+                    storeSearch: null,
+                }
+            },
+            created() {
+                this.loadData().then(response => {
+                    var temp_repo = this.findRepoByName('Map Banner');
+                    if(temp_repo) {
+                        try {
+                            this.pageBanner = temp_repo.images[0];
+                        } catch(e) {
+                            
+                        }
+                    } else {
+                        this.pageBanner = { "image_url": "https://via.placeholder.com/1920x300" }
+                    }
+
+                    this.getSVGMap;
+                    this.dataLoaded = true;
+                });
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    "property",
+                    "timezone",
+                    "findRepoByName",
+                    "processedStores",
+                ]),
+                allStores() {
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                    });
+                    var initZoom = {};
+                    initZoom.svgmap_region = "init";
+                    initZoom.z_coordinate = 1;
+                    initZoom.x = 0.5;
+                    initZoom.y = 0.5;
+                    initZoom.zoom = 1;
+                    all_stores.push(initZoom)
+                    return all_stores
+                },
+                getSVGMap() {
+                    var mapURL = "https://www.mallmaverick.com" + this.property.svgmap_url.split("?")[0];
+                    return mapURL
+                },
+                floorList() {
+                    var floor_list = [];
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getSVGMap;
+                    floor_1.z_index = 1;
+                    floor_1.show = true;
+                    floor_list.push(floor_1);
+                    return floor_list;
+                }
+            },
+            methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                onOptionSelect(option) {
+                    this.$nextTick(function() {
+                        this.storeSearch = ""
+                    });
+                    this.$refs.mapplic_ref.showLocation(option.svgmap_region);
+                },
+                dropPin(store) {
+                    this.$refs.mapplic_ref.showLocation(store.svgmap_region);
+                }
+            }
+        });
+    });
+</script>
