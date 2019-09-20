25e35fcdbbabbc30248577527e901755c8ee1a20
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-20 12:50:42 -0400

    Northgate Shopping Centre 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -109,12 +109,13 @@
                         this.currentPromo.name_short = _.truncate(this.currentPromo.name, { 'length': 21, 'separator': ' ' });
                         this.currentPromo.name_short_2 = _.truncate(this.currentPromo.name_2, { 'length': 21, 'separator': ' ' });
             
-                        // if(_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
-                        //     this.currentPromo.promo_image_url_abs = this.currentPromo.store.store_front_url_abs
-                        // }
-                        // if(_.includes(this.currentPromo.promo_image2_url_abs, 'missing')) {
-                        //     this.currentPromo.promo_image2_url_abs = ""
-                        // }
+                        // if missing image, display store logo
+                        if(_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
+                            this.currentPromo.promo_image_url_abs = this.currentPromo.store.store_front_url_abs
+                        }
+                        if(_.includes(this.currentPromo.promo_image2_url_abs, 'missing')) {
+                            this.currentPromo.promo_image2_url_abs = ""
+                        }
                         
                         // if (_.includes(this.currentPromo.promo_image_url_abs, 'missing')) {
                         //     if (value.promotionable_type == "Store") {
