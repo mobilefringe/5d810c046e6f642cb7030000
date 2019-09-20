f1757a1b15e772c8321d16020ec010922234032f
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-20 16:31:56 -0400

    Northgate Shopping Centre 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -44,6 +44,7 @@
             		    </div>
             		    <div class="margin_30">
                             <div class="map_search_container">
+                                <label class="accessibility">Search Here</label>
                                 <search-component v-model="storeSearch" :list="processedStores" :suggestion-attribute="suggestionAttribute" @select="onOptionSelect" :placeholder="$t('stores_page.find_your_store')">
                                     <template slot="item" scope="option">
                                         <article class="media"><p>{{ option.data.name }}</p></article>
