fe8cc1400571889c2b7135ec045786433c5f3722
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-20 16:15:27 -0400

    Northgate Shopping Centre 1.0  /accessibility.vue

--- accessibility.vue
+++ accessibility.vue
@@ -13,12 +13,12 @@
         			</div>
         		</div>	<!--page_header ends-->
         		
-        		<section class="accessibility site_container">
+        		<section class="accessibility_container site_container">
         		    <div>
         		        <img src="currentPage.image_url" alt="">
         		    </div>
         		    
-        		    <div>
+        		    <div class="site_container">
         				<div class="pages_content about_content">
         				    <div class="page_body description_text text_left" v-if="currentPage" v-html="currentPage.body"></div>
             
