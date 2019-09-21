750850414fb304edc2ae20290903d658e51bb998
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-20 23:39:10 -0400

    Northgate Shopping Centre 1.0  /search_results.vue

---
+++ search_results.vue
@@ -0,0 +1,267 @@
+<!--<template>-->
+<!--	<div>-->
+<!--		<loading-spinner v-if="!dataLoaded"></loading-spinner>-->
+<!--        <transition name="fade">-->
+<!--            <div v-if="dataLoaded" v-cloak>-->
+<!--                <div class="site_container">-->
+<!--                    <div class="pages_content">-->
+<!--                		<div class="page_container text-left" v-if="searchResults && searchResults.length > 0" id="searchResults">-->
+<!--                            <p class="search_result_title">Found {{searchResults.length}} results matching "{{searchQuery}}"</p>-->
+<!--                            <div class="row result_container_row" v-for="(result,index) in searchResults" :key="index">-->
+<!--                                <div v-if="result.is_store" class="col-sm-2 store_details_image center-block">-->
+<!--                                    <div v-if="result.image_url && _.includes(result.image_url,'missing') || (!result.image_url && _.includes(result.store.store_front_url_abs, 'missing')) || (result.image_url && _.includes(result.store.store_front_url_abs, 'missing'))">-->
+<!--                                        <div class="no_logo">-->
+<!--                                            <img src="//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1536092029690/transparent_logo.png">-->
+<!--                                            <h4 class="store_details_name">-->
+<!--                                                <span v-if="result.store_front_url_abs">{{result.name}}</span>-->
+<!--                                                <span v-else>{{ result.store.name }}</span>-->
+<!--                                            </h4>-->
+<!--                                        </div>    -->
+<!--                                    </div> -->
+<!--                                    <div v-else>-->
+<!--                                        <img v-if="result.store" class="result_logo" :src="result.store.store_front_url_abs"/>-->
+<!--                                        <img v-else-if="result.store_front_url_abs" class="result_logo" :src="result.store_front_url_abs"/>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                                <div v-else class="col-sm-2 store_details_image center-block">-->
+<!--                                    <img class="result_logo" src="//codecloud.cdn.speedyrails.net/sites/5bbfac0c6e6f6411b3040000/image/png/1539190868000/mm_default.png"/>    -->
+<!--                                </div>-->
+<!--                                <div class="col-sm-10 search_result_content">-->
+<!--                                    <h4 class="bold">{{result.name}}</h4>-->
+<!--                                    <p>{{ getCategory(result) }}</p>-->
+<!--                                    <p>{{truncated(result.description)}}</p>-->
+<!--                                    <router-link v-if="result.store_front_url_abs" class="result_link hvr-icon-forward" :to="{ name: 'storeDetails', params:{ id:result.slug }}">-->
+<!--                                        <div class="results_btn animated_btn">View Store Details</div>-->
+<!--                                    </router-link>-->
+<!--                                    <router-link v-else-if="result.promo_image_url_abs" class="result_link hvr-icon-forward" :to="{ name: 'promotionDetails', params: { id: result.slug }}">-->
+<!--                                        <div class="results_btn animated_btn">View Promotion Details</div>-->
+<!--                                    </router-link>-->
+<!--                                    <router-link v-else-if="result.event_image_url_abs" class="result_link hvr-icon-forward" :to="{ name: 'eventDetails', params: { id: result.slug }}">-->
+<!--                                        <div class="results_btn animated_btn">View Event Details</div>-->
+<!--                                    </router-link>-->
+<!--                                    <router-link v-else-if="result.jobable_id" class="result_link hvr-icon-forward" :to="{ name: 'jobDetails', params: { id: result.slug }}">-->
+<!--                                        <div class="results_btn animated_btn">View Job Details</div>-->
+<!--                                    </router-link>-->
+<!--                                </div>-->
+<!--                            </div>                -->
+<!--                		</div>-->
+<!--                        <div class="page_container text-left" v-else> -->
+<!--                            <p>Sorry, there are no results that match your search.</p>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--	            </div>-->
+<!--            </div>-->
+<!--        </transition>-->
+<!--    </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--    define(["Vue", "vuex", "jquery"], function(Vue, Vuex, $) {-->
+<!--        return Vue.component("search-results", {-->
+            template: template, // the variable template will be injected
+<!--            data() {-->
+<!--                return {-->
+<!--                    searchResults: null,-->
+<!--                    searchQuery: null,-->
+<!--                    dataLoaded: true,-->
+<!--                }-->
+<!--            },-->
+<!--            beforeRouteUpdate(to, from, next) {-->
+<!--                this.$nextTick(function() {-->
+<!--                    this.updateResults();-->
+<!--                });-->
+<!--                next();-->
+<!--            },-->
+<!--            created() {-->
+<!--                this.updateResults();-->
+<!--                if (this.$route.params.results == null && this.$route.params.results == undefined) {-->
+<!--                    this.$router.push("/");-->
+<!--                }-->
+<!--            },-->
+<!--            computed: {-->
+<!--                ...Vuex.mapGetters([-->
+<!--                    'property',-->
+<!--                    'findCategoryById',-->
+<!--                ])-->
+<!--            },-->
+<!--            methods: {-->
+<!--                truncated(string) {-->
+<!--                    return _.truncate(string, {-->
+<!--                        length: 150,-->
+<!--                        separator: " "-->
+<!--                    });-->
+<!--                },-->
+<!--                updateResults() {-->
+<!--                    if (-->
+<!--                        this.$route.query.searchQuery !== null &&-->
+<!--                        this.$route.query.searchQuery !== undefined &&-->
+<!--                        this.$route.query.searchQuery.length > 0-->
+<!--                    ) {-->
+<!--                        if (-->
+<!--                            this.$route.params.results !== null &&-->
+<!--                            this.$route.params.results !== undefined &&-->
+<!--                            Array.isArray(this.$route.params.results)-->
+<!--                        ) {-->
+<!--                            this.searchResults = this.$route.params.results;-->
+<!--                            this.searchQuery = this.$route.query.searchQuery;-->
+<!--                        }-->
+<!--                    } else {-->
+<!--                        this.$router.push("/");-->
+<!--                    }-->
+<!--                },-->
+<!--                getCategory(result) {-->
+<!--                    console.log(result)-->
+<!--                    if (result.is_store) {-->
+<!--                        if (result.store) {-->
+<!--                            var storeCategory = result.store.categories[0];-->
+<!--                            category = this.findCategoryById(storeCategory)-->
+<!--                            return category.name    -->
+<!--                        } else {-->
+<!--                            var storeCategory = result.categories[0];-->
+<!--                            category = this.findCategoryById(storeCategory)-->
+<!--                            return category.name    -->
+<!--                        }-->
+                        
+<!--                    } else {-->
+                        
+<!--                    }-->
+<!--                }-->
+<!--            }-->
+<!--        });-->
+<!--    });-->
+<!--</script>-->
+
+<template>
+	<div>
+        <div v-if="dataLoaded" v-cloak>
+            <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+    			<div class="site_container">
+    				<div class="header_content">
+    					<h1>Search Results</h1>
+    					<h2 style="display:none;">Scroll to  view page details</h2>
+    					<h3 style="display:none;">View page details</h3>
+    				</div>
+    			</div>
+    		</div>
+            <div class="site_container margin_30">
+        		<div class="text-left" v-if="searchResults && searchResults.length > 0" id="searchResults">
+                    <p class="search_result_title">Found {{searchResults.length}} results matching "{{searchQuery}}"</p>
+        			<div v-for="(result,index) in searchResults" :key="index">
+                        <div class="row result_container_row">
+                            <div v-if="result.is_store" class="col-sm-3 store_details_image center-block">
+                                <div v-if="checkResultImage(result)">
+                                    <div class="no_logo">
+                                        <img src="//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1536092029690/transparent_logo.png">
+                                        <p class="store_details_name">
+                                            <span v-if="result.store_front_url_abs">{{result.name}}</span>
+                                            <span v-else>{{ result.store.name }}</span>
+                                        </p>
+                                    </div>    
+                                </div> 
+                                <div v-else>
+                                    <img v-if="result.store" class="result_logo" :src="result.store.store_front_url_abs"/>
+                                    <img v-else-if="result.store_front_url_abs" class="result_logo" :src="result.store_front_url_abs"/>
+                                </div>
+                            </div>
+                            <div v-else class="col-sm-3 store_details_image center-block">
+                                <img class="result_logo" :src="property.default_logo_url"/>    
+                            </div>
+                            <div class="col-sm-9 search_result_content">
+                                <h4 class="event_name caps">{{result.name}}</h4>
+                                <p class="event_desc">{{truncated(result.description)}}</p>
+                                <router-link v-if="result.store_front_url_abs" class="result_link event_learn_more" :to="{ name: 'storeDetails', params:{ id:result.slug }}">View Store Details</router-link>
+                                <router-link v-else-if="result.promo_image_url_abs" class="result_link event_learn_more" :to="{ name: 'promotionDetails', params: { id: result.slug }}">View Promotion Details</router-link>
+                                <router-link v-else-if="result.event_image_url_abs" class="result_link event_learn_more" :to="{ name: 'eventDetails', params: { id: result.slug }}">View Event Details</router-link>
+                                <router-link v-else-if="result.jobable_id" class="result_link event_learn_more" :to="{ name: 'jobDetails', params: { id: result.slug }}">View Job Details</router-link>
+                            </div>
+                        </div>                
+                    </div>
+        		</div>
+                <div class="page_container text-left col-md-12" v-else> 
+                    <h4>Sorry, there are no results that match your search. Please try again.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex", "jquery"], function(Vue, Vuex, $) {
+        return Vue.component("search-results", {
+            template: template, // the variable template will be injected
+            data() {
+                return {
+                    searchResults: null,
+                    searchQuery: null,
+                    dataLoaded: true,
+                }
+            },
+            beforeRouteUpdate(to, from, next) {
+                this.$nextTick(function() {
+                    this.updateResults();
+                });
+                next();
+            },
+            created() {
+                this.updateResults();
+                if (this.$route.params.results == null && this.$route.params.results == undefined) {
+                    this.$router.push("/");
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'findRepoByName'
+                ]),
+                pageBanner() {
+                    var pageBanner = null;
+                    var temp_repo = this.findRepoByName('Pages Banner');
+                    if (temp_repo && temp_repo.images) {
+                        pageBanner = temp_repo.images[0];
+                    } else {
+                        pageBanner = {};
+                        pageBanner.image_url = "";
+                    }
+                    return pageBanner;
+                }
+            },
+            methods: {
+                truncated(string) {
+                    return _.truncate(string, {
+                        length: 150,
+                        separator: " "
+                    });
+                },
+                updateResults() {
+                    if (
+                        this.$route.query.searchQuery !== null &&
+                        this.$route.query.searchQuery !== undefined &&
+                        this.$route.query.searchQuery.length > 0
+                    ) {
+                        if (
+                            this.$route.params.results !== null &&
+                            this.$route.params.results !== undefined &&
+                            Array.isArray(this.$route.params.results)
+                        ) {
+                            this.searchResults = this.$route.params.results;
+                            this.searchQuery = this.$route.query.searchQuery;
+                        }
+                    } else {
+                        this.$router.push("/");
+                    }
+                },
+                checkResultImage(result){
+                    if(result.store_front_url_abs){
+                        // this is a store with no image
+                        if(_.includes(result.store_front_url_abs,'missing')){
+                            return true
+                        }
+                    } else if(result.store && _.includes(result.store.store_front_url_abs,'missing')){
+                        return true
+                    }
+                    return false
+                }
+            }
+        });
+    });
+</script>
