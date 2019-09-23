e1c59d9c74d2f31da7c81380f60ec8ccc304360e
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-23 12:53:31 -0400

    Northgate Shopping Centre 1.0  /event_gallery_details.vue

--- event_gallery_details.vue
+++ event_gallery_details.vue
@@ -6,7 +6,7 @@
                 <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
         			<div class="site_container">
         				<div class="header_content caps">
-    						<h1>{{ $t("jobs_page.jobs") }}</h1>
+    						<h1>test</h1>
         				</div>
         			</div>
         		</div>
