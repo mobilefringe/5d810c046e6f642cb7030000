ef83e4c2c969cafbe5130aea543451723453d673
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-23 12:48:29 -0400

    Northgate Shopping Centre 1.0  /event_gallery_details.vue

--- event_gallery_details.vue
+++ event_gallery_details.vue
@@ -6,75 +6,126 @@
                 <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
         			<div class="site_container">
         				<div class="header_content caps">
-    						<h1>HEADER</h1>
+    						<h1>HEADER HERE</h1>
         				</div>
         			</div>
         		</div>
-            
-        		<div class="site_container">
-
-        		    <div v-masonry transition-duration="0.3s" item-selector=".grid-item" > 
-                        <div v-masonry-tile class="item">
-                           <!-- <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">-->
-                        	  <!--  <div v-if="feature.description" class="feature_item_container" >-->
-                        			<!--<img :src="feature.image_url" alt="">-->
-                        			<!--<div class="feature_item_info" >-->
-                        			<!--	<div class="feature_item_content">-->
-                        			<!--		<p v-if="feature.description">{{ feature.name }}</p>-->
-                        			<!--		<h3>{{ feature.name }}</h3>-->
-                        			<!--		<a class="feature_item_link" :href="feature.url">-->
-                        			<!--		    <div class="feature_item_more animated_btn" :aria-label="feature.name">{{feature.description}}</div>-->
-                        			<!--		</a>-->
-                        			<!--	</div>-->
-                        			<!--</div>-->
-                        	  <!--  </div>-->
-                        	  <!--  <div v-else-if="!feature.isSocial" class="feature_item_container">-->
-                        	  <!--      <a :href="feature.url">-->
-                           <!-- 			<img :src="feature.image_url" :alt="feature.name">-->
-                            			
-                           <!-- 			<div class="feature_item_info">-->
-                            			
-                           <!-- 			</div>-->
-                            			
-                           <!-- 		</a>-->
-                        	  <!--  </div>-->
-                           <!-- </div>-->
-                        </div>
-                    </div>
-                    <!--<div v-masonry transition-duration="0.3s" item-selector=".grid-item" class="visible_phone">-->
-                    <!--    <div v-masonry-tile class="item">-->
-                    <!--        <div v-for="feature in feature_items" :class="'grid-item ' + feature.masonry_class ">-->
-                    <!--    	    <div v-if="feature.name && feature.description" class="feature_item_container">-->
-                    <!--    	        <a :href="feature.url">-->
-                    <!--        			<img :src="feature.image_url" :alt="feature.name">-->
-                    <!--        			<div class="feature_item_info" >-->
-                    <!--        				<div class="feature_item_content">-->
-                    <!--        					<p>{{ feature.name }}</p>-->
-                    <!--        					<h3>{{ feature.description }}</h3>-->
-                    <!--        					<div class="feature_item_more" :aria-label="feature.name">View Details</div>-->
-                    <!--        				</div>-->
-                    <!--        			</div>-->
-                    <!--        		</a>-->
-                    <!--    	    </div>-->
-                    <!--    	    <div v-else class="feature_item_container">-->
-                    <!--    	        <a :href="feature.url">-->
-                    <!--        			<img :src="feature.image_url" :alt="feature.name">-->
-                    <!--        			<div class="feature_item_info"></div>-->
-                    <!--        		</a>-->
-                    <!--    	    </div>-->
-                    <!--        </div>-->
-                    <!--    </div>-->
-                    <!--</div>-->
-
-
-        		</div>
-        		
-        		
-       
-        	</div>
-        </transition>    		
+        		<div class="margin_60"></div>
+                <div v-if="dataLoaded" v-cloak class="site_container">
+                    
+            		
+    		    </div>
+    		</div>
+		</transition>
 	</div>
 </template>
+
+// <script>
+//     define(['Vue', 'vuex', 'moment', 'vue-lazy-load'], function(Vue, Vuex, moment, VueLazyload) {
+//         Vue.use(VueLazyload);
+//         return Vue.component("job-details-component", {
+//             template: template, // the variable template will be injected,
+//             data: function() {
+//                 return {
+//                     dataLoaded: false,
+//                     pageBanner:null,
+//                     currentJob: null,
+//                     storeJobs : null
+//                 }
+//             },
+//             props:['id', 'locale'],
+//             beforeRouteUpdate(to, from, next) {
+//                 this.updateCurrentJob(this.id);
+//                 next();
+//             },
+//             created(){
+//                 this.loadData().then(response => {
+//                     var temp_repo = this.findRepoByName('Jobs Banner');
+//                     if(temp_repo) {
+//                         try {
+//                             this.pageBanner = temp_repo.images[0];
+//                         } catch(e) {
+                            
+//                         }
+//                     } else {
+//                         this.pageBanner = { "image_url": "" }
+//                     }
+//                     this.updateCurrentJob(this.id);
+//                     this.dataLoaded = true;
+//                 });
+//             },
+//             computed: {
+//                 ...Vuex.mapGetters([
+//                     'property',
+//                     'timezone',
+//                     'findRepoByName',
+//                     'findJobBySlug',
+//                     'findCategoryById'
+//                 ])
+//             },
+//             methods: {
+//                 updateCurrentJob (id) {
+//                     this.currentJob = this.findJobBySlug(id);
+//                     if (this.currentJob != null || this.currentJob != undefined){
+//                         var property_name = this.property.name
+//                         if (this.currentJob.store != null && this.currentJob.store != undefined){
+//                             this.currentJob.store_name = this.currentJob.store.name;
+//                             this.currentJob.store_phone = this.currentJob.store.phone
+//                             this.currentJob.store_website = this.currentJob.store.website
+                        
+//                             // STORE LOGO
+//                             if (_.includes(this.currentJob.store.store_front_url_abs, 'missing')) {
+//                                 this.currentJob.no_logo = true
+//                             }
+
+//                             else {
+//                                 this.currentJob.no_logo = false
+//                                 this.currentJob.store_url = this.currentJob.store.image_url;
+//                             }
+
+//                         } else if (this.currentJob.store == null || this.currentJob.store == undefined) {
+//                             this.currentJob.store = {};
+//                             this.currentJob.no_logo = false;
+//                             this.currentJob.store_url =  this.property.default_logo_url;
+//                             this.currentJob.store_name = property_name;
+//                         }
+//                     } else {
+//                         this.$router.replace({ name: '404'});
+//                     }
+//                 },
+//                 loadData: async function() {
+//                     try {
+//                         let results = await Promise.all([this.$store.dispatch("getData", "repos"), this.$store.dispatch("getData", "jobs"), this.$store.dispatch("getData", "categories")]);
+//                     } catch (e) {
+//                         console.log("Error loading data: " + e.message);
+//                     }
+//                 },
+//                 checkJobType(promo) {
+//                     if(this.locale != "en-ca") {
+//                         if(promo.job_type == "Full Time"){
+//                             return "Plein Temps"
+//                         } else if (promo.job_type == "Part Time"){
+//                             return "Mi Temps"
+//                         } else if (promo.job_type == "Full Time/Part Time"){
+//                             return "Plein/Mi Temps"
+//                         } else if (promo.job_type == "Seasonal"){
+//                             return "Travail Saisonnier"
+//                         }
+//                     } else {
+//                         return promo.job_type
+//                     }
+//                 },
+//                 storeCategory(currentJob) {
+//                     if (currentJob.store.length > 0) {
+//                         var currentStoreCategory = currentJob.store.categories[0];
+//                         category = this.findCategoryById(currentStoreCategory);
+//                         return category.name
+//                     }
+//                 }
+//             }
+//         });
+//     });
+// </script>
 
 <script>
     define(["Vue", "vuex", "vue!vue-slick", "js-cookie", "masonry", "vue-masonry-plugin", "vue!mapplic-map", "moment", "moment-timezone"], function(Vue, Vuex, slick, Cookies, masonry, VueMasonryPlugin, MapplicComponent, moment, tz) {
