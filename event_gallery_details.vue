e0627047821835514f49fbc536b01049d8f38383
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-23 12:29:53 -0400

    Northgate Shopping Centre 1.0  /event_gallery_details.vue

--- event_gallery_details.vue
+++ event_gallery_details.vue
@@ -3,18 +3,12 @@
         <loader v-if="!dataLoaded"></loader>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
-        		<div class="banner_div">
-        			<div class="home_banner_container">
-        				<slick ref="slick" :options="slickOptions">
-        					<div class="" v-for="banner in banners" v-if="banners">
-        						<div class="home_banner" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }">
-        						    <div class="home_banner_content" v-if="banner.name && banner.description">
-        						        <h1>{{ banner.name }}</h1>
-        						        <h2>{{ banner.description }}</h2>
-        						    </div>   
-        						</div>
-        					</div>
-        				</slick>
+        		<div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        			<div class="site_container">
+        				<div class="header_content caps">
+    						<!--<h1>{{ $t("jobs_page.jobs") }}</h1>-->
+    						<h1>EVENT NAME HERE</h1>
+        				</div>
         			</div>
         		</div>
         		<div class="site_container">
@@ -191,7 +185,7 @@
             
             props:['id', 'locale'],
             beforeRouteUpdate(to, from, next) {
-                this.updateCurrentGallery(this.id);
+                this.updateCurrentJob(this.id);
                 next();
             },
             data: function() {
@@ -213,7 +207,7 @@
                         this.pageBanner = { "image_url": "" }
                     }
                     
-                    this.updateCurrentGallery(this.id);
+                    this.updatecurrentEvent(this.id);
                     this.dataLoaded = true;
                 });
             },
@@ -222,32 +216,160 @@
                     'property',
                     'findRepoByName',
                     'processedGalleries',
-                    'findGalleryById',
-                    'findGalleryBySlug'
-                ])
-            },
-            methods: {
-                updateCurrentGallery(id) {
-                    this.currentGallery = this.findGalleryBySlug(id);
-                    if (this.currentGallery != null || this.currentGallery != undefined){
-                        var property_name = this.property.name
-                        if (this.currentGallery.name != null && this.currentGallery.name != undefined){
-                            this.currentGallery.gallery_name = this.currentGallery.name;
-                            this.currentGallery.gallery_description = this.currentGallery.description
-                            // this.currentJob.store_website = this.currentJob.store.website
-                        
-
-
-                        // } else if (this.currentJob.store == null || this.currentJob.store == undefined) {
-                        //     this.currentJob.store = {};
-                        //     this.currentJob.no_logo = false;
-                        //     this.currentJob.store_url =  this.property.default_logo_url;
-                        //     this.currentJob.store_name = property_name;
+                    // 'findGalleryById',
+                    // 'findGalleryBySlug',
+                    
+                    'findEventBySlug'
+                ]),
+                banners () {
+                    var banners = [];//this.$store.state.banners;
+                    _.forEach(this.$store.state.banners, function(value, key) {
+                        var today = new Date();
+                        var start = new Date (value.start_date);
+                        if (start <= today){
+                            if (value.end_date){
+                                var end = new Date (value.end_date);
+                                if (end >= today){
+                                    banners.push(value);  
+                                }
+                            } else {
+                                banners.push(value);
+                            }
                         }
+                    })
+                    return _.orderBy(banners, ['position'], ['asc']);
+                },
+                feature_items () {
+                    var features = _.slice(this.$store.state.feature_items, 0, 6);
+                    _.forEach(features, function(value, key) {
+                        if ( _.includes([1], key) ) {
+                            value.masonry_class = "grid-item--width2";
+                        } else if ( _.includes([2], key) ){
+                            value.masonry_class = "grid-item--width2";
+                        } else if ( _.includes([3], key) ){
+                            value.masonry_class = "grid-item--height2";
+                        } else {
+                            value.masonry_class = " ";
+                        }
+                        if (_.includes(value.name , "Social Media")) {
+                            value.isSocial = true
+                        } else {
+                            value.isSocial = false
+                        }
+                    });
+                    return features;
+                },
+                programs () {
+                    var features = _.slice(this.$store.state.feature_items, 6, 8); 
+                    _.forEach(features, function(value, key) {
+                        if ( _.includes([1], key)) {
+                            value.program_class = "last";
+                        }
+                    });
+                    return features
+                },
+                // mobile_feature_items () {
+                //     var features = _.slice(this.$store.state.feature_items, 0, 6);
+                //     _.forEach(features, function(value, key) {
+                //         if ( _.includes([1], key) ) {
+                //             value.masonry_class = "grid-item--width2";
+                //             value.text_class = " ";
+                //             value.border = " ";
+                //         } else if ( _.includes([2], key) ){
+                //             value.masonry_class = "grid-item--width2";
+                //             value.text_class = "add_black";
+                //             value.border = " ";
+                //         } else if ( _.includes([3], key) ){
+                //             value.masonry_class = "grid-item--height2";
+                //             value.text_class = " ";
+                //             value.border = " ";
+                //         } else if ( _.includes([4], key) ){
+                //             value.text_class = " ";
+                //             value.border = "full_border";
+                //         } else if ( _.includes([5], key) ){
+                //             value.text_class = "add_black";
+                //             value.border = " ";
+                //             value.last_item = true;
+                //         } else {
+                //             value.masonry_class = " ";
+                //             value.text_class = " ";
+                //             value.border = " ";
+                //             value.last_item = false;
+                //         }
+                //     });
+                //     features = _.sortBy(features, [function(o) { return o.mobile_order; }]);
+                //     return features;
+                // }
+                allStores() {
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                    });
+                    var initZoom = {};
+                    initZoom.svgmap_region = "init";
+                    initZoom.z_coordinate = 1;
+                    initZoom.x = 0.5;
+                    initZoom.y = 0.5;
+                    initZoom.zoom = 1;
+                    all_stores.push(initZoom)
+                    return all_stores
+                },
+                getSVGMap() {
+                    var mapURL = "https://www.mallmaverick.com" + this.property.svgmap_url.split("?")[0];
+                    return mapURL
+                },
+                floorList() {
+                    var floor_list = [];
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getSVGMap;
+                    floor_1.z_index = 1;
+                    floor_1.show = true;
+                    floor_list.push(floor_1);
+                    return floor_list;
+                },
+                todaysHours() {
+                    var timezone = this.timezone
+                    var regHours = this.getPropertyHours;
+                    var holHours = this.getPropertyHolidayHours;
+                    var hours = [];
+                    // First check if there is a holiday today
+                    _.forEach(holHours, function(value) {
+                        var today = moment().format("YYYY-MM-DD");
+                        var holiday_date = moment(value.holiday_date).tz(timezone).format("YYYY-MM-DD");
+                        if (today == holiday_date) {
+                            if (value.is_closed) {
+                                value.is_open = false;
+                            } else {
+                                value.is_open = true;
+                            }
+                            hours.push(value);
+                        }
+                    });
+                    // If there is no holiday today, check for today's hours
+                    if (hours.length > 0) {
+                        return hours;
                     } else {
-                        this.$router.replace({ name: '404'});
+                        _.forEach(regHours, function(value) {  
+                            var today = moment().day();
+                            if ( today == value.day_of_week ) {
+                                var time = moment().tz(timezone).format('HH:mm');
+                                var opens = moment(value.open_time).tz(timezone).format('HH:mm');
+                                var closes = moment(value.close_time).tz(timezone).format('HH:mm');
+                                if (time > opens && time < closes) {
+                                    value.is_open = true;
+                                } else {
+                                    value.is_open = false;
+                                }
+                                hours.push(value);
+                            }         
+                        });
+                        return hours;
                     }
                 },
+            },
+            methods: {
                 loadData: async function() {
                     try {
                         let results = await Promise.all([
@@ -259,6 +381,14 @@
                         console.log("Error loading data: " + e.message);
                     }
                 }
+                // updateCurrentJob (id) {
+                //     this.currentJob = this.findJobBySlug(id);
+                //     if (this.currentJob != null || this.currentJob != undefined){
+                //         //customize json
+                //     } else {
+                //         this.$router.replace({ name: '404'});
+                //     }
+                // },
             }
         })
     })
