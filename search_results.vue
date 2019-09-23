bc6a8350bfc1e45da4ebc6320903833091a5d67a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-22 22:15:26 -0400

    Northgate Shopping Centre 1.0  /search_results.vue

--- search_results.vue
+++ search_results.vue
@@ -27,7 +27,7 @@
                                 </div> 
                                 <div v-else>
                                     <img v-if="result.store" class="result_logo" :src="result.store.store_front_url_abs" alt="" />
-                                    <img v-else-if="result.store_front_url_abs" class="result_logo" :src="result.store_front_url_abs"/>
+                                    <img v-else-if="result.store_front_url_abs" class="result_logo" :src="result.store_front_url_abs" alt="" />
                                 </div>
                             </div>
                             <div v-else class="col-sm-3 store_details_image center-block">
