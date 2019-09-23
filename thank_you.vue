be549daf8193e9672665fb7790f30e70244158a0
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-23 00:07:54 -0400

    Northgate Shopping Centre 1.0  /thank_you.vue

---
+++ thank_you.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="page_container" id="thank_you_container">
+    <div class="row">
+    </div>
+        <div class="row">
+            Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+        </div>
+    </div>
+</template>
+<style>
+    #thank_you_container div{
+        padding: 20px;
+    }
+</style>
+<script>
+    define(["Vue"], function(Vue) {
+        return Vue.component("thank-you-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    title: "title"
+                }
+            }
+        });
+    });
+</script>
