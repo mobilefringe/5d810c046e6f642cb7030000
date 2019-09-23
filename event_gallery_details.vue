eca928abfb38a346ab72afa14ca015c5d9e95e9d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-23 13:16:51 -0400

    Northgate Shopping Centre 1.0  /event_gallery_details.vue

--- event_gallery_details.vue
+++ event_gallery_details.vue
@@ -138,7 +138,22 @@
                     'processedGalleries',
                     'findGalleryById',
                     'findGalleryBySlug'
-                ])
+                ]),
+                galleries() {
+                    var allGalleries = this.processedGalleries;
+                    var event_name = null;
+                    var firstImageUrl = null;
+                    var _this = this;
+                    _.forEach(allGalleries, function(value, key) {
+                        if (value.fashion_images.length > 0) {
+                            value.firstImageUrl = _this.property.mm_host + value.fashion_images[0].photo_url
+                            if(value.name != null && value.name != undefined) {
+                                // value.event_name = value.event.name;
+                            }
+                         }
+                    });
+                    return allGalleries
+                }
             },
             methods: {
                 loadData: async function() {
