c1ed4e3389e9e2560c00c7c624728550535890a8
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-24 14:03:35 -0400

    Northgate Shopping Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -61,7 +61,7 @@
 						<ul>
 						    <li class="menu_item"  tabindex=0 v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>
-						        <span  tabindex=0 v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>
+						        <span v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>
 						        <ul v-if="item.sub_menu">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
