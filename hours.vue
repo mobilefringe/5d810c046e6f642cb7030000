e3e5ba5cbc55ba686603820dfe32d9880c5e6987
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-25 16:33:15 -0400

    Northgate Shopping Centre 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -60,7 +60,7 @@
                                 <div class="col-md-12 col-lg-4">
                                     <p class="bold caps">{{ hour.holiday_name }}</p>
                                 </div>
-                                <div class="col-sm-6 col-lg-3">
+                                <div class="col-sm-6 col-lg-4">
                                     <p>{{ hour.holiday_date | moment("MMM D, YYYY", timezone) }}</p>    
                                 </div>
                                 <div class="col-sm-6 col-lg-4">
