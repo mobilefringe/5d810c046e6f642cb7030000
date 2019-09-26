d1179368c8eb9efe10b661cbf4e7ff9b88d7aac8
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-26 13:38:04 -0400

    Northgate Shopping Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -44,7 +44,7 @@
 				</div>
 				<div class="hidden_phone">
 				    <div class="header_search_container">
-					    <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left">
+					    <search-component v-if="headerReady" :list="searchList" placeholder="Search" :suggestion-attribute="suggestionAttribute" :keys="keys" v-model="search_result" @select="onOptionSelect" :autocomplete="false" :minMatchCharLength="3" :tokenize="true" class="text-left" required>
                             <template slot="item" scope="option" class="manual">
                                 <article class="media">
                                     <p>{{ option.data.name }}</p>
