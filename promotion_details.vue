238152fa6cb392442d6958fe2ee3be53dc805fd1
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-09-27 00:25:57 -0400

    Northgate Shopping Centre 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -39,7 +39,7 @@
             				    <div v-if="locale=='en-ca'" v-html="currentPromo.rich_description"></div>
 				                <div v-else v-html="currentPromo.rich_description_2"></div>
             				</div>
-            				        				        <social-sharing :url="$root.shareURL('events',currentEvent.slug)" :title="currentEvent.title" :description="currentEvent.body" :quote="_.truncate(currentEvent.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="currentEvent.image_url" inline-template >
+            				        				        <social-sharing :url="$root.shareURL('events',currentPromo.slug)" :title="currentPromo.title" :description="currentPromo.body" :quote="_.truncate(currentPromo.description, {'length': 99})" :twitter-user="$root.twitter_user" :media="currentPromo.image_url" inline-template >
             						<div class="blog-social-share">
             							<div class="event_details_social_share">
             							    <p class="bold">Share:</p>
