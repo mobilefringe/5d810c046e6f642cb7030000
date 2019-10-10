1a460f8e7b0e184ac8e32f1d574c9f2db5ea5dd9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-10 19:12:26 -0400

    Northgate Shopping Centre 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -62,7 +62,7 @@
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>
 						        <span tabindex=0 v-if="item.sub_menu != undefined">{{$t(item.name)}}</span>
-						        <ul v-if="item.sub_menu" class="subdropdown">
+						        <ul v-if="item.sub_menu" class="subdropdown has-submenu">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
 						            </li>
@@ -272,6 +272,22 @@
                 },
             },
             methods: {
+                var menuItems = document.querySelectorAll('li.has-submenu');
+                Array.prototype.forEach.call(menuItems, function(el, i){
+                	el.querySelector('a').addEventListener("click",  function(event){
+                		if (this.parentNode.className == "has-submenu") {
+                			this.parentNode.className = "has-submenu open";
+                			this.setAttribute('aria-expanded', "true");
+                		} else {
+                			this.parentNode.className = "has-submenu";
+                			this.setAttribute('aria-expanded', "false");
+                		}
+                		event.preventDefault();
+                		return false;
+                	});
+                });
+                
+                
                 googleTranslateInit: function() {
                     let checkIfGoogleLoaded = setInterval(() => {
                         if (google.translate.TranslateElement != null) {
